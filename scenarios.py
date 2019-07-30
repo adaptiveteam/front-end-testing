6f5490d8cbdd6f5103a756c4e19cf4188f922eb4
Christopher Creel <chris.creel@adaptive.team>
2019-07-29 21:15:12 -0400

    further refactoring to make this more testable

--- scenarios.py
+++
@@ -1,8 +0,0 @@
-import components
-from time import sleep
-
-
-def set_up(driver):
-    components.sign_in(driver)
-    driver.implicitly_wait(5)
-    components.select_adaptive(driver)
--- test_set_up.py
+++ test_set_up.py
@@ -1,38 +1,62 @@
 from unittest import TestCase
 import os
-import scenarios
+import logging
 import urllib3
 import certifi
+import components
 from selenium import webdriver
-import tracemalloc
+from sauceclient import SauceClient
 
-tracemalloc.start()
 
-class TestSet_up(TestCase):
-    def get_desired_capabilities(self, name):
+class TestSetUp(TestCase):
+    drivers = {}
+    sauce_username = os.environ['SAUCE_USERNAME']
+    sauce_access_key = os.environ['SAUCE_ACCESS_KEY']
+    remote_url = os.environ['REMOTE_URL']
+
+    def get_driver(self, scenario, site_to_test):
+        rv = None
         desired_cap = {
             'platform': os.environ['PLATFORM'],
             'browserName': os.environ['BROWSER'],
             'version': os.environ['BROWSER_VERSION'],
             'build': "none",
-            'name': name,
+            'name': scenario,
             'username': os.environ['SAUCE_USERNAME'],
             'accessKey': os.environ['SAUCE_ACCESS_KEY'],
         }
-        return desired_cap
+        if scenario not in self.drivers.keys():
+            try:
+                self.drivers[scenario] = webdriver.Remote(command_executor=self.remote_url,
+                                                          desired_capabilities=desired_cap)
+                self.drivers[scenario].get(site_to_test)
+                rv = self.drivers[scenario]
+            except Exception as e:
+                logging.error(e)
+                rv = None
+        return rv
 
-    def setUp(self):
-        desired_cap = self.get_desired_capabilities('Log-In and Set-Up')
-        self.http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-        self.driver = webdriver.Remote(command_executor=os.environ['REMOTE_URL'], desired_capabilities=desired_cap)
-        self.driver.get(os.environ['SITE_TO_TEST'])
 
+    @classmethod
+    def setUpClass(cls):
+        cls.http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+        cls.sauce_client = SauceClient(cls.sauce_username, cls.sauce_access_key)
 
-    def tearDown(self):
-        self.driver.quit()
-
+    @classmethod
+    def tearDownClass(cls):
+        for driver in cls.drivers.values():
+            driver.quit()
 
     def test_set_up(self):
-        scenarios.set_up(self.driver)
-        return True
+        scenario = "setup"
+        driver = self.get_driver(scenario, os.environ["SITE_TO_TEST"])
+        result = "failed"
+        try:
+            components.sign_in(driver, os.environ['WORKSPACE'], os.environ['USER_EMAIL'], os.environ['USER_PASSWORD'])
+            driver.implicitly_wait(5)
+            components.select_adaptive(driver)
+            result = "passed"
+        except Exception as e:
+            logging.error(e)
 
+        self.sauce_client.jobs.update_job(driver.session_id, passed=result, name=scenario)
